5ff1d1562fb96d658bef29533cd13b203fd813db
Avdhesh Garodia <avdheshgarodia@gmail.com>
2016-04-26 17:45:56 -0500

    YELPPgit add index.jsgit add index.js

--- index.js
+++ index.js
@@ -8,6 +8,72 @@ app.get('/', function(request, response) {
   response.send('YAY HIIIII!')
 })
 
+app.get('/yelp', function(request, response) {
+  	//var yelp = require("node-yelp");
+ 
+ 
+	// var client = yelp.createClient({
+	//   oauth: {
+	//     "consumer_key": "wrkA_dfOUA1KkQ3vk5Oz5g",
+	//     "consumer_secret": "uqjeTS9lqAlhrTpzmMsti5kLxHE",
+	//     "token": "gQKUJvZoLTbQO0IYDQ0LuCLtTowcqBEC",
+	//     "token_secret": "Z_9p9w87sQ7RHHG6-OFiLF7-0bc"
+	//   },
+	  
+	//   // Optional settings: 
+	//   httpClient: {
+	//     maxSockets: 25  // ~> Default is 10 
+	//   }
+	// });
+	 
+	 
+	// client.search({
+	//   terms: "food",
+	//   location: "Champaign, Il",
+	//   limit: "100",
+	//   sort: "2"
+
+	// }).then(function (data) {
+	//   var names = [];
+	//   var businesses = data.businesses;
+	//   var location = data.region;
+	//   for(i=0;i<businesses.length;i++){
+	//   	names.push(businesses[i].name)
+	//   }
+	//   response.send(names)
+	   
+	// });
+	var Yelp = require('yelp');
+
+	var yelp = new Yelp({
+ 	 	consumer_key: 'wrkA_dfOUA1KkQ3vk5Oz5g',
+  		consumer_secret: 'uqjeTS9lqAlhrTpzmMsti5kLxHE',
+  		token: 'gQKUJvZoLTbQO0IYDQ0LuCLtTowcqBEC',
+  		token_secret: 'Z_9p9w87sQ7RHHG6-OFiLF7-0bc',
+	});
+
+	yelp.search({ term: 'food', location: 'Champaign, Il', sort: 0, limit: 20})
+	.then(function (data) {
+  		//response.send(data);
+  		//response.send('test');
+
+  	  var names = [];
+	  var businesses = data.businesses;
+	  var location = data.region;
+	  for(i=0;i<businesses.length;i++){
+	  	names.push(businesses[i].name)
+	  }
+	  response.send(names)
+	   
+
+	})
+
+
+
+
+
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
