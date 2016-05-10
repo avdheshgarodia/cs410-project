fc330abc43f794af9d41b1b9da102f9ae55dbef8
Avdhesh Garodia <avdheshgarodia@gmail.com>
2016-05-09 23:29:28 -0500

    Formatted Better

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
             $('#submit_button').click();
     });
 
-
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
     $("#submit_button").on("click", function () {
         // $.get("/twitter",function(data,status)
@@ -37,22 +37,33 @@ $(document).ready(function () {
             //var len = (10<data.length)? 10: data.length
             for (i = 0; i < data.length; i++) {
                 if (data[i].sentiment != 0) {
-                    var tweet = "tweet : " + data[i].tweet + "; sentiment : " + data[i].sentiment
+                    var tweet = "<h6><b>Tweet</b></h6> : " + data[i].tweet + " ---- <b>Sentiment</b> : " + data[i].sentiment
                     $("#trending").append('<li>' + tweet + '</li>')
                 }
             }
 
         });
         $.get("/weather", function (data, status) {
+            var d = new Date();
+            var n = d.getDay();
+            
             console.log("Data: " + data + "\nStatus: " + status);
-            var current_conditions = "temp : " + data.current_observation.temp_c + "; feel like : " + data.current_observation.feelslike_c;
+            var current_conditions = "    Temperature : " + data.current_observation.temp_c + " -- Feels Like : " + data.current_observation.feelslike_c;
+            $("#forecast").append('<h6><b>Today</h6></b>');
             $("#forecast").append('<li>' + current_conditions + '</li>');
+            $("#forecast").append('<h6><b>4 Day Forecast</h6></b>');
+            
             for (i = 0; i < (data.forecast.txt_forecast.forecastday.length); i += 2) {
+                $("#forecast").append('<h6><b>'+days[(n+((i/2)+1))%7]+'</b></h6>');
                 var daytime = data.forecast.txt_forecast.forecastday[i].fcttext;
                 var night = data.forecast.txt_forecast.forecastday[i + 1].fcttext;
-                var forecast = "daytime : " + daytime + "; night : " + night
+                var forecast = "Morning : " + daytime;
+                var forecast1 = "Night : " + night;
+
                 $("#forecast").append('<li>' + forecast + '</li>');
+                $("#forecast").append('<li>' + forecast1 + '</li>');
             }
+            
         });
 
     });
